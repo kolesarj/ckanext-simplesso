5d0d9c55d55b8e9bd899e45d185f660cf286bf63
kolesarj <john@kolesar.ca>
2019-04-02 14:30:42 +0000

    ESAS changes

--- ckanext/simplesso/plugin.backup.py
+++ ckanext/simplesso/plugin.backup.py
@@ -7,7 +7,7 @@ import pylons.config as config
 
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-import logging
+
 
 class SimpleSSOPlugin(plugins.SingletonPlugin):
     '''A CKAN plugin that enables SSO using a simple header parameter.
@@ -21,9 +21,7 @@ class SimpleSSOPlugin(plugins.SingletonPlugin):
 
         '''
         toolkit.add_template_directory(config, 'templates')
-        self.header_parameter = config.get('ckan.simplesso.header_parameter', 'partyID')
-        self.header_username = config.get('ckan.simplesso.header_username', 'username')
-        self.header_email = config.get('ckan.simplesso.header_email', 'email')
+        self.header_parameter = config.get('ckan.simplesso.header_parameter', 'user-id')
         self.email_domain = config.get('ckan.simplesso.email_domain')
 
     def login(self):
@@ -35,44 +33,19 @@ class SimpleSSOPlugin(plugins.SingletonPlugin):
         If a logged in user is found, set toolkit.c.user to be their user name.
 
         '''
-        logger = logging.getLogger(__name__)
-        logger.debug('ESAS: HEADER SENT TO CKAN')
-        logger.debug(self.header_parameter)
-        logger.debug(toolkit.request.headers)
-        if self.header_parameter in toolkit.request.headers:
-            logger.debug('PartyID')
-            logger.debug(toolkit.request.headers.get('partyID'))
-            userid = toolkit.request.headers.get(self.header_parameter).lower()
-            username = toolkit.request.headers.get(self.header_username).lower()
-            email = toolkit.request.headers.get(self.header_email).lower()
-            user = get_user_by_userid(userid)
 
-            if user:
-                # Check if ESAS email for user has changed.
-                # If it has changed then update user email to match
-                # CKAN is not system of record for email. 
-                # Changes as needed to match ESAS header.
-                if not email=user['email']
-                    logger.log('ESAS: A user account has changed email.')
-                    user=toolkit.get_action('user_update')(a
-                        context={'ignore_auth': True},
-                        data_dict={'id':userid,
-                                   'email': email})
-				 
+        if self.header_parameter in toolkit.request.headers:
+            userid = toolkit.request.headers.get(self.header_parameter).lower()
+            email = userid + "@" + self.email_domain
+            user = get_user_by_username(userid)
+
             if not user:
-                # Check if user email is already associated with an existing account.
-                # If there are duplicate emails raise error
-                email_check = get_user_by_email(email)
-                if email_check:
-                    logger.error('ESAS: An existing account already has this email')  
                 # A user with this username doesn't yet exist in CKAN
                 # - so create one.
-                logger.log('ESAS: user not found. Creating new CKAN user.')
                 user = toolkit.get_action('user_create')(
                     context={'ignore_auth': True},
                     data_dict={'email': email,
-                               'id': userid,
-                               'name': username,
+                               'name': userid,
                                'password': generate_password()})
             toolkit.c.user = user['name']
 
@@ -93,20 +66,6 @@ def get_user_by_username(username):
     # way to search for users by email address using the API yet.
     import ckan.model
     user = ckan.model.User.get(username)
-
-    if user:
-        user_dict = toolkit.get_action('user_show')(data_dict={'id': user.id})
-        return user_dict
-    else:
-        return None
-
-def get_user_by_userid(userid):
-    '''Return the CKAN user with the given userid.
-
-    :rtype: A CKAN user dict
-
-    '''
-    user = ckan.model.User.get(userid)
 
     if user:
         user_dict = toolkit.get_action('user_show')(data_dict={'id': user.id})
--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ setup(
     ''',
     classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     keywords='',
-    author='Jørgen Abrahamsen',
-    author_email='jorgen.abrahamsen@gmail.com',
-    url='jorgenabrahamsen.com',
+    author='',
+    author_email='',
+    url='',
     license='',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     namespace_packages=['ckanext', 'ckanext.simplesso'],
